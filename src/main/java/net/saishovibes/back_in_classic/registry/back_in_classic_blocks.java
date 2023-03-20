d0fdc8a8d2e735feb15ebda8e04f9bd356ffc0ee
Mantis <Brickbuilder876@gmail.com>
2023-03-20 22:49:07 +0000

    Model Fixes, Loot Tables, Lang Fixes

--- src/main/java/net/saishovibes/back_in_classic/registry/back_in_classic_blocks.java
+++ src/main/java/net/saishovibes/back_in_classic/registry/back_in_classic_blocks.java
@@ -15,25 +15,25 @@ public interface back_in_classic_blocks {
     Map<Identifier, Block> BLOCKS = new LinkedHashMap<>();
 
     //BLOCKS
-    Block WAX_BLOCK = register(new Identifier(Back_In_Classic.MODID, "wax_block"), new Block(FabricBlockSettings.copyOf(Blocks.HONEYCOMB_BLOCK).nonOpaque()));
-    Block CLASSIC_NETHER_PORTAL = register(new Identifier(Back_In_Classic.MODID, "classic_nether_portal"), new Block(FabricBlockSettings.copyOf(Blocks.HONEYCOMB_BLOCK).nonOpaque()));
-    Block COGWHEEL = register(new Identifier(Back_In_Classic.MODID, "cogwheel"), new Block(FabricBlockSettings.copyOf(Blocks.REDSTONE_BLOCK).nonOpaque()));
-    Block DIRT_SLAB = register(new Identifier(Back_In_Classic.MODID, "dirt_slab"), new Block(FabricBlockSettings.copyOf(Blocks.PETRIFIED_OAK_SLAB).nonOpaque()));
+    Block WAX_BLOCK = register(new Identifier(Back_In_Classic.MODID, "wax_block"), new SlimeBlock(FabricBlockSettings.copyOf(Blocks.HONEYCOMB_BLOCK)));
+    Block CLASSIC_NETHER_PORTAL = register(new Identifier(Back_In_Classic.MODID, "classic_nether_portal"), new Block(FabricBlockSettings.copyOf(Blocks.HONEYCOMB_BLOCK).luminance(9)));
+    Block COGWHEEL = register(new Identifier(Back_In_Classic.MODID, "cogwheel"), new Block(FabricBlockSettings.copyOf(Blocks.REDSTONE_BLOCK)));
+    Block DIRT_SLAB = register(new Identifier(Back_In_Classic.MODID, "dirt_slab"), new SlabBlock(FabricBlockSettings.copyOf(Blocks.PETRIFIED_OAK_SLAB).nonOpaque()));
     Block GEAR = register(new Identifier(Back_In_Classic.MODID, "gear"), new Block(FabricBlockSettings.copyOf(Blocks.REDSTONE_WIRE).nonOpaque()));
-    Block GLOWING_OBSIDIAN = register(new Identifier(Back_In_Classic.MODID, "glowing_obsidian"), new Block(FabricBlockSettings.copyOf(Blocks.OBSIDIAN).nonOpaque()));
+    Block GLOWING_OBSIDIAN = register(new Identifier(Back_In_Classic.MODID, "glowing_obsidian"), new Block(FabricBlockSettings.copyOf(Blocks.OBSIDIAN).luminance(10)));
     Block INF_LAVA = register(new Identifier(Back_In_Classic.MODID, "infinite_lava"), new Block(FabricBlockSettings.copyOf(Blocks.LAVA).nonOpaque()));
     Block INF_WATER = register(new Identifier(Back_In_Classic.MODID, "infinite_water"), new Block(FabricBlockSettings.copyOf(Blocks.WATER).nonOpaque()));
     Block REV_GEAR = register(new Identifier(Back_In_Classic.MODID, "inverse_gear"), new Block(FabricBlockSettings.copyOf(Blocks.REDSTONE_WIRE).nonOpaque()));
-    Block NETHER_REACTOR = register(new Identifier(Back_In_Classic.MODID, "nether_reactor_core"), new Block(FabricBlockSettings.copyOf(Blocks.GLOWSTONE).nonOpaque()));
-    Block SOD_BLOCK = register(new Identifier(Back_In_Classic.MODID, "sod_block"), new Block(FabricBlockSettings.copyOf(Blocks.MOSS_BLOCK).nonOpaque()));
-    Block SOD_SLAB = register(new Identifier(Back_In_Classic.MODID, "sod_slab"), new Block(FabricBlockSettings.copyOf(Blocks.OAK_SLAB).nonOpaque()));
-    Block CRUSHED_GRAVEL = register(new Identifier(Back_In_Classic.MODID, "crushed_gravel"), new Block(FabricBlockSettings.copyOf(Blocks.GRAVEL).nonOpaque()));
+    Block NETHER_REACTOR = register(new Identifier(Back_In_Classic.MODID, "nether_reactor_core"), new Block(FabricBlockSettings.copyOf(Blocks.GLOWSTONE).luminance(12)));
+    Block SOD_BLOCK = register(new Identifier(Back_In_Classic.MODID, "sod_block"), new Block(FabricBlockSettings.copyOf(Blocks.MOSS_BLOCK)));
+    Block SOD_SLAB = register(new Identifier(Back_In_Classic.MODID, "sod_slab"), new SlabBlock(FabricBlockSettings.copyOf(Blocks.OAK_SLAB).nonOpaque()));
+    Block CRUSHED_GRAVEL = register(new Identifier(Back_In_Classic.MODID, "crushed_gravel"), new FallingBlock(FabricBlockSettings.copyOf(Blocks.GRAVEL)));
 
     //PLANTS
-    Block BLUE_ROSE = register(new Identifier(Back_In_Classic.MODID, "blue_rose"), new Block(FabricBlockSettings.copyOf(Blocks.BLUE_ORCHID).nonOpaque()));
-    Block PAEONIA = register(new Identifier(Back_In_Classic.MODID, "paeonia_flower"), new Block(FabricBlockSettings.copyOf(Blocks.PINK_TULIP).nonOpaque()));
-    Block RED_ROSE = register(new Identifier(Back_In_Classic.MODID, "red_rose"), new Block(FabricBlockSettings.copyOf(Blocks.POPPY).nonOpaque()));
-    Block SHRUB = register(new Identifier(Back_In_Classic.MODID, "shrub"), new Block(FabricBlockSettings.copyOf(Blocks.DEAD_BUSH).nonOpaque()));
+    Block BLUE_ROSE = register(new Identifier(Back_In_Classic.MODID, "blue_rose"), new PlantBlock(FabricBlockSettings.copyOf(Blocks.BLUE_ORCHID).nonOpaque()));
+    Block PAEONIA = register(new Identifier(Back_In_Classic.MODID, "paeonia_flower"), new PlantBlock(FabricBlockSettings.copyOf(Blocks.PINK_TULIP).nonOpaque()));
+    Block RED_ROSE = register(new Identifier(Back_In_Classic.MODID, "red_rose"), new PlantBlock(FabricBlockSettings.copyOf(Blocks.POPPY).nonOpaque()));
+    Block SHRUB = register(new Identifier(Back_In_Classic.MODID, "shrub"), new PlantBlock(FabricBlockSettings.copyOf(Blocks.DEAD_BUSH).nonOpaque()));
 
     //CAULDRONS
     Block AWKWARD_CAULDRON = register(new Identifier(Back_In_Classic.MODID, "awkward_cauldron"), new Block(FabricBlockSettings.copyOf(Blocks.CAULDRON).nonOpaque()));
